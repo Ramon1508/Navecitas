371c8c67f577503fcbe6caeebfbe59d028dde32f
Ramon1508 <valenzuela_luna@hotmail.com>
2019-03-05 18:33:07 -0700

    Commit inicial

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float deltaMove = 10f;
+    public GameObject prefab;
+    public float fuerzabala = 100f;
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody>();
+        //rigidbody.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        //Rotate();
+        //Shoot();
+    }
+    void Shoot(){/* 
+        if (Input.GetKeyUp(KeyCode.UpArrow)){
+            var NuevoDisparo = Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), transform.rotation);
+            NuevoDisparo.GetComponent<Rigidbody>().AddForce(Vector3.forward * fuerzabala, ForceMode.Impulse);
+            //NuevoDisparo.GetComponent<Transform>().Rotate(new Vector3(90f, 0f, 0f) * Time.deltaTime);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            var NuevoDisparo = Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, transform.position.z - 1), transform.rotation);
+            NuevoDisparo.GetComponent<Rigidbody>().AddForce(Vector3.back * fuerzabala, ForceMode.Impulse);
+        }  
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            var NuevoDisparo = Instantiate(prefab, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z), transform.rotation);
+            NuevoDisparo.GetComponent<Rigidbody>().AddForce(Vector3.left * fuerzabala, ForceMode.Impulse);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow)) 
+        {
+            var NuevoDisparo = Instantiate(prefab, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), transform.rotation);
+            NuevoDisparo.GetComponent<Rigidbody>().AddForce(Vector3.right * fuerzabala, ForceMode.Impulse);
+        }*/
+    }
+    void Rotate()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 0f) * Time.deltaTime);
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.up * deltaMove * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.down * deltaMove * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.left * deltaMove * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.right * deltaMove * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/NaveGiro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveGiro : MonoBehaviour
+{
+    public float GiroX = 1;
+    public float GiroY = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Girar();
+    }
+    void Girar() {
+        if (Input.GetKey(KeyCode.W))
+            transform.Rotate(new Vector3(GiroY, 0f, 0f) * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Rotate(new Vector3(-GiroY, 0f, 0f) * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0f, 0f, GiroX) * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(new Vector3(0f, 0f, -GiroX) * Time.deltaTime);
+        Debug.Log(transform.rotation.z);
+    }
+}
